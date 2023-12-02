bc0da5cbd99ee8ab5e28eb961b06160f59e138c1
okeke-N <140330982+okeke-N@users.noreply.github.com>
2023-12-02 08:12:07 +0100

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -3,100 +3,86 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
 
-//User model
-const User =require('../models/User');
+const User = require('../models/User')
 
+//Login page
+router.get('/login',(req, res) => res.render('login'));
 
-//login 
-router.get("/login", (req, res)=> res.render('login'));
+//Rgister page
+router.get('/register',(req, res) => res.render('register'));
 
-
-//register
-router.get("/register", (req, res)=> res.render('register'));
-
-//register handle
 router.post('/register', (req, res)=>{
-                    const{ name, email, password} = req.body;
-                    let errors =[];
+                    const {name, email, password, password2} = req.body;
+                    let errors =[]
 
-                    //check required fields
-                    if( !email || !password){
+                    //check email fields
+                    if(!name || !email || !password || !password2){
                                         errors.push({msg: 'Please fill in all fields'})
+                    }
 
+                    //check password
+                    if(password !== password2){
+                                        errors.push({msg: 'Passwords do not match'})
                     }
                     //check password length
                     if(password.length < 6){
-                                        errors.push({msg: 'Password should be at least six chracters'})
+                                        errors.push({msg:'Passwords should be at least six characters'})
                     }
-                    if (errors.length < 0){
+                    if(errors.length > 0){
+
                                         res.render('register',{
                                                             errors,
                                                             name,
                                                             email,
-                                                            password
-                                        });
-
+                                                            password,
+                                                            password2
+                                        })
                     }else{
                                         //Validation passed
                                         User.findOne({email:email})
                                         .then(user =>{
-                                                            if(user){
-                                                                                //user exist
-                                                                                errors.push({msg: 'Email is already registered'})
+                                                            if(user){ 
+                                                                                errors.push({msg:'Email already exist'})
                                                                                 res.render('register',{
                                                                                                     errors,
                                                                                                     name,
                                                                                                     email,
-                                                                                                    password
+                                                                                                    password,
+                                                                                                    password2
                                                                                 });
                                         
                                                             }else{
-                                                                                const newUser =new User({
+                                                                                const newUser = new User({
                                                                                                     name,
                                                                                                     email,
                                                                                                     password
+
                                                                                 });
-                                                                                //harshed password
-                                                                                bcrypt.genSalt(10, (err, salt)=>bcrypt.harsh(newUser.password, salt, (err, hash) =>{
-                                                                                                    if(err) throw err;
-
-                                                                                                    //hashed password
-                                                                                                    newUser.password =hash;
-
-                                                                                                    //Save user
+                                                                                //hashed password
+                                                                                bcrypt.genSalt(10, (err, salt)=>
+                                                                                bcrypt.hash(newUser.password, salt, (err, hash)=>{
+                                                                                                    if (err)throw err
+                                                                                                    newUser.password = hash;
                                                                                                     newUser.save()
-                                                                                                    .then(user =>{
-                                                                                                                        req.flash('success_msg', 'Welcome aboard chatmate')
-                                                                                                                        res.redirect('/users/login');
-                                                                                                    })
-                                                                                                    .catch(err => console.log(err));
-
+                                                                                                     .then(user =>{
+                                                                                                                        req.flash('success_msg',  'Welcome aboard chatmate')
+                                                                                                                        res.redirect('/users/login')
+                                                                                                     })
+                                                                                                     .catch(err => console.log(err))
                                                                                 }))
 
                                                             }
                                         });
-
                     }
-
-                    
 });
-//Login Handle
-
+//Handle Login
 router.post('/login', (req, res, next)=>{
                     passport.authenticate('local', {
-                                        successRedirect: '/dashboard',
-                                        failureRedirect: 'users/login',
-                                        failureFlash: true,
+                                        successRedirect:'/dashboard',
+                                        failureRedirect:'/users/login',
+                                        failureFlash: true
                     })(req, res, next);
-
-});
-
-//Logout handle
-router.get('/logout', (req, res, next)=>{
-                    req.logout();
-                    req.flash('success_msg', 'sucessfiully loged out');
-                    res.redirect('/users/login');
-});
+})
 
 
 module.exports = router
