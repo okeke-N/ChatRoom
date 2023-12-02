4962e2b85bc4e6d1071025eed7a87b9f0f5c806a
okeke-N <140330982+okeke-N@users.noreply.github.com>
2023-12-02 08:14:04 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -9,7 +9,7 @@ const UserSchema = new mongoose.Schema({
                                         type:String,
                                         required: true
                     },
-                    pssword:{
+                    password:{
                                         type:String,
                                         required: true
                     },
